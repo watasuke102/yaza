16b3eb15e8e9a27de0d7c4b43f16bf276e26b2ad
watasuke102 <watasuke102@gmail.com>
2025-01-10 10:05:17 +0900

    update: create session

--- src/remote.cpp
+++ src/remote.cpp
@@ -4,55 +4,46 @@
 #include <zen-remote/loop.h>
 #include <zen-remote/server/peer-manager.h>
 
+#include <cassert>
+#include <exception>
+#include <functional>
 #include <memory>
+#include <optional>
 
 #include "common.hpp"
+#include "zen-remote/server/channel.h"
+#include "zen-remote/server/gl-buffer.h"
+#include "zen-remote/server/peer.h"
+#include "zen-remote/server/session.h"
 
+namespace yaza::remote {
 namespace {
 class LogSink : public zen::remote::ILogSink {
   void Sink(zen::remote::Severity severity, const char* /*pretty_function*/,
       const char* file, int line, const char* format, va_list vp) override {
     using zen::remote::Severity;
-    yaza::log::Severity s = yaza::log::Severity::INFO;
+    log::Severity s = log::Severity::INFO;
     switch (severity) {
       case Severity::DEBUG:
-        s = yaza::log::Severity::DEBUG;
+        s = log::Severity::DEBUG;
         break;
       case Severity::INFO:
-        s = yaza::log::Severity::INFO;
+        s = log::Severity::INFO;
         break;
       case Severity::WARN:
-        s = yaza::log::Severity::WARN;
+        s = log::Severity::WARN;
         break;
       case Severity::ERROR:
       case Severity::FATAL:
-        s = yaza::log::Severity::ERR;
+        s = log::Severity::ERR;
         break;
       default:
         break;
     }
-    yaza::log::vprintf("ZenRemote", s, file, line, format, vp);
+    log::vprintf("ZenRemote", s, file, line, format, vp);
   }
 };
 
-int loop_callback(int fd, uint32_t mask, void* data) {
-  using zen::remote::FdSource;
-  uint32_t remote_mask = 0;
-  if (mask & WL_EVENT_READABLE) {
-    remote_mask |= FdSource::kReadable;
-  }
-  if (mask & WL_EVENT_WRITABLE) {
-    remote_mask |= FdSource::kWritable;
-  }
-  if (mask & WL_EVENT_HANGUP) {
-    remote_mask |= FdSource::kHangup;
-  }
-  if (mask & WL_EVENT_ERROR) {
-    remote_mask |= FdSource::kError;
-  }
-  static_cast<zen::remote::FdSource*>(data)->callback(fd, remote_mask);
-  return 1;
-}
 class Loop : public zen::remote::ILoop {
  public:
   DISABLE_MOVE_AND_COPY(Loop);
@@ -73,8 +64,26 @@ class Loop : public zen::remote::ILoop {
     if (source->mask & FdSource::kError) {
       mask |= WL_EVENT_ERROR;
     }
-    source->data =
-        wl_event_loop_add_fd(wl_loop_, source->fd, mask, loop_callback, source);
+
+    auto handle_loop_callback = [](int fd, uint32_t mask, void* data) {
+      uint32_t remote_mask = 0;
+      if (mask & WL_EVENT_READABLE) {
+        remote_mask |= FdSource::kReadable;
+      }
+      if (mask & WL_EVENT_WRITABLE) {
+        remote_mask |= FdSource::kWritable;
+      }
+      if (mask & WL_EVENT_HANGUP) {
+        remote_mask |= FdSource::kHangup;
+      }
+      if (mask & WL_EVENT_ERROR) {
+        remote_mask |= FdSource::kError;
+      }
+      static_cast<zen::remote::FdSource*>(data)->callback(fd, remote_mask);
+      return 1;
+    };
+    source->data = wl_event_loop_add_fd(
+        wl_loop_, source->fd, mask, handle_loop_callback, source);
   }
   void RemoveFd(zen::remote::FdSource* source) override {
     wl_event_source_remove(static_cast<wl_event_source*>(source->data));
@@ -87,33 +96,97 @@ class Loop : public zen::remote::ILoop {
   wl_event_loop* wl_loop_;
 };
 
+class Session {
+ public:
+  DISABLE_MOVE_AND_COPY(Session);
+  explicit Session(const std::shared_ptr<zen::remote::server::IPeer>& peer,
+      wl_event_loop* wl_loop, std::function<void()> on_disconnect)
+      : wl_loop_(wl_loop)
+      , peer_id_(peer->id())
+      , session_(zen::remote::server::CreateSession(
+            std::make_unique<Loop>(this->wl_loop_))) {
+    if (!this->session_->Connect(peer)) {
+      LOG_WARN("Failed to connect with peer %lu", peer->id());
+      throw std::exception();
+    }
+    // FIXME: calling on_disconnect() in zen-remote may raises SIGSEGV
+    this->session_->on_disconnect.Connect(on_disconnect);
+    this->channel_ = zen::remote::server::CreateChannel(this->session_);
+  }
+  ~Session() {
+    LOG_DEBUG("(Session destructor, peer id=%lu)", peer_id_);
+  }
+  [[nodiscard]] uint64_t id() const {
+    return this->peer_id_;
+  }
+
+ private:
+  wl_event_loop* wl_loop_;
+  uint64_t       peer_id_;
+
+  std::shared_ptr<zen::remote::server::ISession> session_;
+  std::shared_ptr<zen::remote::server::IChannel> channel_;
+};
+
 class Remote {
  public:
   DISABLE_MOVE_AND_COPY(Remote);
-  explicit Remote(std::unique_ptr<zen::remote::ILoop> loop)
-      : peer_manager_(zen::remote::server::CreatePeerManager(std::move(loop))) {
+  explicit Remote(wl_event_loop* loop)
+      : wl_loop_(loop)
+      , current_session_(std::nullopt)
+      , peer_manager_(zen::remote::server::CreatePeerManager(
+            std::make_unique<Loop>(loop))) {
     this->peer_manager_->on_peer_discover.Connect([this](uint64_t peer_id) {
-      auto peer_data = this->peer_manager_->Get(peer_id);
-      if (!peer_data) {
+      auto peer = this->peer_manager_->Get(peer_id);
+      if (!peer) {
         return;
       }
-      LOG_DEBUG(
-          "peer is discovered: %lu, %s", peer_id, peer_data->host().c_str());
+      LOG_DEBUG("(PeerManager) peer is discovered: id=%lu, host=`%s`", peer_id,
+          peer->host().c_str());
+      if (this->current_session_.has_value()) {
+        return;
+      }
+      try {
+        this->current_session_ = std::make_unique<Session>(
+            std::move(peer), this->wl_loop_, [this]() {
+              LOG_DEBUG("disconnection handler for ISession");
+              if (this->current_session_.has_value()) {
+                this->disconnect();
+              }
+            });
+      } catch (std::exception& _) {
+        this->current_session_ = std::nullopt;
+        return;
+      }
+      LOG_DEBUG("session is established with peer id=%lu", peer_id);
     });
-    this->peer_manager_->on_peer_lost.Connect([](uint64_t peer_id) {
-      LOG_DEBUG("peer disconnected : %lu", peer_id);
+
+    this->peer_manager_->on_peer_lost.Connect([this](uint64_t peer_id) {
+      LOG_DEBUG("(PeerManager) peer is lost      : id=%lu", peer_id);
+      if (!this->current_session_.has_value()) {
+        return;
+      }
     });
   }
   ~Remote() = default;
 
  private:
+  wl_event_loop* wl_loop_;
+
+  std::optional<std::unique_ptr<Session>>            current_session_;
   std::unique_ptr<zen::remote::server::IPeerManager> peer_manager_;
+
+  void disconnect() {
+    assert(this->current_session_.has_value());
+    LOG_DEBUG(
+        "disconnecting session (id=%lu)", this->current_session_->get()->id());
+    this->current_session_ = std::nullopt;
+  }
 };
 }  // namespace
 
-namespace yaza::remote {
 void create(wl_event_loop* loop) {
   zen::remote::InitializeLogger(std::make_unique<LogSink>());
-  auto* remote = new Remote(std::make_unique<Loop>(loop));
+  auto* remote = new Remote(loop);
 }
 }  // namespace yaza::remote
