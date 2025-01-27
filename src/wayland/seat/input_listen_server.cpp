f2ca848e4a61b4d0f000fe19baef6294968eb25c
watasuke102 <watasuke102@gmail.com>
2025-01-27 16:57:15 +0900

    update: InputListenServer>use non-blocking socket API

--- src/wayland/seat/input_listen_server.cpp
+++ src/wayland/seat/input_listen_server.cpp
@@ -2,11 +2,19 @@
 #include "wayland/seat/input_listen_server.hpp"
 
 #include <arpa/inet.h>
+#include <asm-generic/socket.h>
+#include <fcntl.h>
 #include <netinet/in.h>
+#include <pthread.h>
+#include <sys/epoll.h>
+#include <sys/fcntl.h>
 #include <sys/socket.h>
 
 #include <cerrno>
+#include <chrono>
+#include <csignal>
 #include <cstdint>
+#include <cstdio>
 #include <cstring>
 #include <thread>
 
@@ -15,7 +23,7 @@
 
 namespace yaza::wayland::seat {
 InputListenServer::InputListenServer(Seat* seat) : seat_(seat) {
-  // NOLINTNEXTLINE(cppcoreguidelines-macro-usage)
+// NOLINTNEXTLINE(cppcoreguidelines-macro-usage)
 #define BAIL(err_prefix)                                                       \
   LOG_ERR(err_prefix ": %s", std::strerror(errno));                            \
   return
@@ -23,6 +31,13 @@ InputListenServer::InputListenServer(Seat* seat) : seat_(seat) {
   this->socket_ = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
   if (this->socket_ == -1) {
     BAIL("Failed to create a socket");
+  }
+  if (fcntl(this->socket_, F_SETFL, O_NONBLOCK) == -1) {
+    BAIL("Failed to set O_NONBLOCK to socket");
+  }
+  int opt = 1;
+  if (setsockopt(this->socket_, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt))) {
+    LOG_WARN("Failed set SO_REUSEADDR to socket: %s", std::strerror(errno));
   }
   sockaddr_in addr;
   std::memset(&addr, 0, sizeof(addr));
@@ -38,30 +53,51 @@ InputListenServer::InputListenServer(Seat* seat) : seat_(seat) {
   }
 
   this->thread_ = std::thread([this] {
+    {
+      sigset_t mask;
+      sigemptyset(&mask);
+      sigaddset(&mask, SIGINT);
+      pthread_sigmask(SIG_BLOCK, &mask, nullptr);
+    }
+
     while (!this->terminate_requested_) {
-      // wait for client
       sockaddr_in client_addr;
-      int client = accept4(this->socket_, nullptr, nullptr, SOCK_CLOEXEC);
+      memset(&client_addr, 0, sizeof(client_addr));
+      socklen_t len = 0;
+      int       client =
+          accept4(this->socket_, (sockaddr*)&client_addr, &len, SOCK_CLOEXEC);
       if (client == -1) {
+        if (errno == EAGAIN || errno == EWOULDBLOCK) {
+          std::this_thread::sleep_for(std::chrono::milliseconds(500));
+          continue;
+        }
         BAIL("Failed to accept a client");
+      }
+      if (fcntl(client, F_SETFL, O_NONBLOCK) == -1) {
+        LOG_WARN("Failed to set O_NONBLOCK to client");
+        close(client);
+        continue;
       }
       LOG_INFO(
           "Connected with input client: %s", inet_ntoa(client_addr.sin_addr));
       this->handle_events(client);
-      close(client);
+      if (close(client) == -1) {
+        LOG_WARN("Failed to close client properly: %s", std::strerror(errno));
+      }
+      LOG_INFO("Disconnected with %s", inet_ntoa(client_addr.sin_addr));
+    }
+    if (close(this->socket_) == -1) {
+      LOG_WARN("Failed to close socket properly: %s", std::strerror(errno));
     }
   });
 
   LOG_INFO("InputListenServer is started (port: %d)", kServerPort);
-#undef BAIL
 }
 
 InputListenServer::~InputListenServer() {
-  // FIXME: this means nothing, proper error handling needed
-  // like not using blocking socket API (epoll)
   this->terminate_requested_ = true;
-  if (this->socket_ != -1) {
-    close(this->socket_);
+  if (this->thread_.joinable()) {
+    this->thread_.join();
   }
 }
 
@@ -72,6 +108,9 @@ void InputListenServer::handle_events(int client) const {
   while (!this->terminate_requested_) {
     ssize_t size = recv(client, buf.data(), sizeof(uint8_t) * buf.size(), 0);
     if (size == -1) {
+      if (errno == EAGAIN || errno == EWOULDBLOCK) {
+        continue;
+      }
       LOG_WARN("Failed to receive data from client: %s", strerror(errno));
       break;
     }
